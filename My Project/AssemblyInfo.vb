2af5e6001a88c1b27d6c6f0054d1b3da618cd5a2
Owen Fujii <dalekrebel@gmail.com>
2024-02-13 20:53:44 -0700

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("RollTheDice")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("RollTheDice")>
+<Assembly: AssemblyCopyright("Copyright © HP 2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c0b5c46f-0712-4865-9185-9c460e6569b6")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ RollTheDice.vb
@@ -0,0 +1,78 @@
+﻿'OwenFujii
+'RCET 2265
+'Spring 2024
+'
+
+
+Option Explicit On
+Option Strict On
+
+
+Module RollTheDice
+
+    Sub Main()
+        RollTrackerTotal()
+        Console.WriteLine(RollTrackerTotal())
+        Console.Read()
+    End Sub
+
+
+    Function RollDice() As Integer
+        Dim diceRoll As Integer
+        Randomize()
+        diceRoll = CInt(Math.Floor((12 - 2 + 1) * Rnd())) + 2
+        Return diceRoll
+    End Function
+
+    Function RollTrackerTotal() As Integer()
+        Dim rollTracker(10) As Integer
+        Dim diceRolled As Integer
+        Dim twoRolled%, threeRolled%, fourRolled%, fiveRolled%
+        Dim sixRolled%, sevenRolled%, eightRolled%, nineRolled%
+        Dim tenRolled%, elevenRolled%, twelveRolled%
+        For i = 1 To 1000
+            diceRolled = RollDice()
+            If diceRolled = 2 Then
+                twoRolled = twoRolled + 1
+                rollTracker(0) = twoRolled
+            ElseIf diceRolled = 3 Then
+                threeRolled = threeRolled + 1
+                rollTracker(1) = threeRolled
+            ElseIf diceRolled = 4 Then
+                fourRolled = fourRolled + 1
+                rollTracker(2) = fourRolled
+            ElseIf diceRolled = 5 Then
+                fiveRolled = fiveRolled + 1
+                rollTracker(3) = fiveRolled
+            ElseIf diceRolled = 6 Then
+                sixRolled = sixRolled + 1
+                rollTracker(4) = sixRolled
+            ElseIf diceRolled = 7 Then
+                sevenRolled = sevenRolled + 1
+                rollTracker(5) = sevenRolled
+            ElseIf diceRolled = 8 Then
+                eightRolled = eightRolled + 1
+                rollTracker(6) = eightRolled
+            ElseIf diceRolled = 9 Then
+                nineRolled = nineRolled + 1
+                rollTracker(7) = nineRolled
+            ElseIf diceRolled = 10 Then
+                tenRolled = tenRolled + 1
+                rollTracker(8) = tenRolled
+            ElseIf diceRolled = 11 Then
+                elevenRolled = elevenRolled + 1
+                rollTracker(9) = elevenRolled
+            ElseIf diceRolled = 12 Then
+                twelveRolled = twelveRolled + 1
+                rollTracker(10) = twelveRolled
+            End If
+
+        Next
+
+        Return rollTracker()
+
+    End Function
+
+
+
+End Module
